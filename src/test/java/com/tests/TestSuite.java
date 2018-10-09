7ef8e67edaae21087f862d950ed02d48f8b215ad
Hori <Hori@DESKTOP-BHPFJ7D>
2018-10-09 15:18:25 +0300

    asd

---
+++ src/test/java/com/tests/TestSuite.java
@@ -0,0 +1,10 @@
+package com.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CreateItemTest.class, BookItemTest.class })
+public class TestSuite {
+}
