2442ffa78478ed8f87ad3cc058be15a142c33e6c
Hori <Hori@DESKTOP-BHPFJ7D>
2018-10-10 16:54:49 +0300

    asd

--- src/test/java/com/tests/CreateCategoryTest.java
+++ src/test/java/com/tests/CreateCategoryTest.java
@@ -1,12 +1,10 @@
 package com.tests;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.steps.api.ApiCategorySteps;
 import com.steps.api.ApiLoginSteps;
-import com.tests.BaseTest;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
