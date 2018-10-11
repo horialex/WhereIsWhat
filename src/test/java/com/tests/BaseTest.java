ac0788a33282a6cbb6ddc041273924a0619c2ac2
Hori <Hori@DESKTOP-BHPFJ7D>
2018-10-11 11:37:35 +0300

    asd

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -3,6 +3,7 @@ package com.tests;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 	@Managed(uniqueSession = true)
@@ -10,7 +11,6 @@ public class BaseTest {
 
 	@Before
 	public void setup() {
-
 //		System.setProperty("http.proxyHost", "localhost");
 //		System.setProperty("http.proxyPort", "8080");
 //		System.setProperty("https.proxyHost", "localhost");
